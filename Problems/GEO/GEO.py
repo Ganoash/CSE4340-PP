348ef0a059f66ce40dfe010a32013bed76b828b9
Kostas Andriopoulos <konstantinosand@gmail.com>
2022-10-06 18:08:20 +0200

    GEO: Add logic for reading input file

--- Problems/GEO/GEO.py
+++ Problems/GEO/GEO.py
@@ -1,13 +1,8 @@
-import numpy as np
-import pyro.distributions as dist
-import pyro.primitives as prim
-from pyro import condition
-import torch
-
-
 class GeoModel:
-    def __init__(self):
+    def __init__(self, heads, reference_times, observed_deformations):
         # initializations
+        
+        # sampling
         self.draw()
 
     def draw(self):
@@ -20,10 +15,25 @@ class GeoModel:
 
 
 def read_from_file(file_location: str):
+    """Read the input data from the given file
+
+    Args:
+        file_location (str): The location of the given input file.
+
+    Returns:
+        (List[int], List[int], List[int]): Three lists of the same length,
+            corresponding to: heads, reference time and observed deformation
+            measurments.
+    """
     with open(file_location) as f:
-        pass
-        # read inputs
-    return None  # inputs
+        heads = [float(h) for h in f.readline().split()]
+        reference_times = [float(h) for h in f.readline().split()]
+        observed_deformations = [float(h) for h in f.readline().split()]
+    if len(heads) != len(reference_times) != len(observed_deformations):
+        raise RuntimeError("Heads, reference times and observed deformations"
+                           " were expected to have the same length but they"
+                           " didn't.")
+    return heads, reference_times, observed_deformations
 
 
 if __name__ == "__main__":
@@ -31,10 +41,11 @@ if __name__ == "__main__":
     file_location = input()
 
     # load the inputs from the input file
-    inputs = read_from_file(file_location)
+    heads, reference_times, observed_deformations = read_from_file(
+        file_location)
 
     # initialize the model
-    model = GeoModel(inputs)
+    model = GeoModel(heads, reference_times, observed_deformations)
 
     # perform inference
     model.run()
